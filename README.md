0e62100a5971ff267104ba6f8362e241fb1a2222
thutinski <tino.hutinski@gmail.com>
2024-01-23 15:18:28 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+test the entire CI/CD process using a demo python application without Jenkins, directly through github actionspipeline
---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Testing new pipeline - 23.01.2024 11:10 from github actions which does building/pushing image to repo, kustomize edit yaml files with new image tags, then the rest is handled by argo"
+
