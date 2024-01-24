ec44aa848a110de510309a08230bb2c8d25c954c
thutinski <tino.hutinski@gmail.com>
2024-01-24 10:31:20 +0100

    check pipeline 031

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Testing new pipeline - 24.01.2024 09:40 from github actions which does building/pushing image to repo, kustomize edit yaml files with new image tags, then the rest is handled by argo"
+    return "Testing new pipeline - 24.01.2024 10:30 from github actions which does building/pushing image to repo, kustomize edit yaml files with new image tags, then the rest is handled by argo"
